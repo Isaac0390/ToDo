a9634cf810ebfa2ed05ef3116d03a33e47d543b5
Isaac Garcia <isaacdy@hotmail.com>
2018-06-14 20:42:51 -0500

    Agregué un formulario con Bootstrap

--- main.rb
+++ main.rb
@@ -1,6 +1,26 @@
 require 'sinatra'
 require './config'
 
+todos = []
+
 get '/' do
-    erb :index
+  @todos = todos
+  erb :index
+end
+
+get '/todos' do
+  @todos = todos
+  erb :index
+end
+
+get '/todos/create' do
+  erb :create
+end
+
+post '/todos' do
+  todos.push({
+    "title": params[:title],
+    "description": params[:description]
+  })
+  redirect '/todos'
 end
